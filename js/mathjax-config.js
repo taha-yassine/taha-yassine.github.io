6b38fa1bab56923f1dc59df223f844313d3075b9
taha-yassine <taha-yassine@users.noreply.github.com>
2022-10-25 17:04:08 +0000

    deploy: taha-yassine/starter-hugo-academic@9d1b9d3d1e4d1c7bd3f14826f9425bd84c4b3daf

--- js/mathjax-config.js
+++ js/mathjax-config.js
@@ -4,23 +4,24 @@
 // - The CommonHTML.linebreaks option is not yet implemented (but may be in a future release)
 // - The TeX.noUndefined.attributes option is not yet implemented (but may be in a future release)
 window.MathJax = {
-  options: {
-    // Don't render math in mindmaps as Markmap has its own math renderer.
-    ignoreHtmlClass: 'markmap',
-  },
-  tex: {
-    inlineMath: [
-      ['$', '$'],
-      ['\\(', '\\)'],
-    ],
-    displayMath: [
-      ['$$', '$$'],
-      ['\\[', '\\]'],
-    ],
-    processEscapes: false,
-    packages: {'[+]': ['noerrors']},
-  },
-  loader: {
-    load: ['[tex]/noerrors'],
-  },
+    options: {
+        // Don't render math in mindmaps as Markmap has its own math renderer.
+        ignoreHtmlClass: 'markmap',
+    },
+    tex: {
+        inlineMath: [
+            ['$', '$'],
+            ['\\(', '\\)'],
+        ],
+        displayMath: [
+            ['$$', '$$'],
+            ['\\[', '\\]'],
+        ],
+        processEscapes: false,
+        packages: { '[+]': ['noerrors'] },
+        tags: 'ams' // Adds support for numbered equations
+    },
+    loader: {
+        load: ['[tex]/noerrors'],
+    },
 };
